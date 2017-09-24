dfcdf693f05704c379d5305cebf5ce3c74b02e10
怪鱼小贩 <18268105345@163.com>
2017-09-24 18:43:32 +0800

    第二周

---
+++ 15002050232_DP/src/week2/validator_ex/AuthenticatorFactory.java
@@ -0,0 +1,21 @@
+package week2.validator_ex;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class AuthenticatorFactory {
+
+
+    public static Authenticator createAuthenticator() {
+
+    }
+
+    private static KeyValueKeeper createKeyValueKeeper() {
+
+    }
+
+    private static Encrypter createEncrypter() {
+
+    }
+}
