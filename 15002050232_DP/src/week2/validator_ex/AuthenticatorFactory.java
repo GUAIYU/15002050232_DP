4367f9ce2d94e2f6589bf85fe0d169d0bd4da643
怪鱼小贩 <18268105345@163.com>
2017-09-25 20:01:52 +0800

    第二次作业完善

--- 15002050232_DP/src/week2/validator_ex/AuthenticatorFactory.java
+++ 15002050232_DP/src/week2/validator_ex/AuthenticatorFactory.java
@@ -6,16 +6,39 @@ import java.util.Properties;
 
 public class AuthenticatorFactory {
 
+    private static Properties sak = new Properties();
+    static {
+        try {
+            sak.load(new FileInputStream("src/week2/validator_ex/auth.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Authenticator createAuthenticator() {
-
+        KeyValueKeeper keeper = createKeyValueKeeper();
+        Encrypter encrypter = createEncrypter();
+        return new Authenticator(keeper, encrypter);
     }
 
     private static KeyValueKeeper createKeyValueKeeper() {
-
+        KeyValueKeeper keeper = null;
+        try {
+            keeper = (KeyValueKeeper) Class.forName( sak.getProperty("KeyValueKeeper") ).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return keeper;
     }
 
     private static Encrypter createEncrypter() {
+        Encrypter encrypter = null;
+        try {
+            encrypter = (Encrypter) Class.forName( sak.getProperty("Encrypter") ).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encrypter;
 
     }
 }
